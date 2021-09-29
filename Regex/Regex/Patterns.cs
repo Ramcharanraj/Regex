6f5d05bf9213d763148d6d66d574a8d85a64b439
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-09-30 01:00:34 +0530

    ADDED:REGEX

---
+++ Regex/Regex/Patterns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex
+{
+    public class Patterns
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9](5)$"; //400000
+
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.validatePinCode("abc123"));
+        }
+    }
+}
